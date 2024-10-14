aa265a4a1af2913cce4c4aad1af09f17fb75a890
k2211326 <k2211326@kingston.ac.uk>
2024-10-14 12:28:48 +0100

    started room generation
    i have started creating a random dungeon room generation, however it needs to be fixed(its not spawning the rooms as children of the grid

---
+++ Assets/RoomTemplates.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+}
+
