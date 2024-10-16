4cf3c33060a0aed6fd09bdae16b0594a77061542
k2211326 <k2211326@kingston.ac.uk>
2024-10-16 09:43:13 +0100

    started fixing the misplaced rooms
    it now works 7/10 times when generating rooms. in other cases the rooms will just spawn slightly off or completely in the wrong place

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -15,12 +15,19 @@ public class RoomSpawner : MonoBehaviour
     private int rand;
     private bool spawned = false;
 
+    private Vector3 fixdown = new Vector3(-6f, -7.5f, 0f);
+    private Vector3 fixup = new Vector3(-6f, 5.5f, 0);
+    private Vector3 fixright = new Vector3(5.5f,-1f, 0f);
+    private Vector3 fixleft = new Vector3(-17.5f, -1f, 0f);
+
     // this needs to be optimised its going to affect my game if it has to search all the objects to find these tags
     private void Start()
     {
       templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
       parentObject = GameObject.FindGameObjectWithTag("RoomSeed").GetComponent<Transform>(); // this is a referance to the grid 
-        Invoke("Spawn", 0.9f);
+        Invoke("Spawn", 1f);
+
+        
     }
     private void Spawn()
     {
@@ -30,28 +37,28 @@ public class RoomSpawner : MonoBehaviour
             {
                 // spawn room with top door
                 rand = Random.Range(0, templates.topRooms.Length);
-                GameObject newObject =Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                GameObject newObject = Instantiate(templates.topRooms[rand], transform.position + fixdown, templates.topRooms[rand].transform.rotation);
                 newObject.transform.SetParent(parentObject);
             }
             else if (openingDirection == 2)
             {
                 //spawn room with bottom door
                 rand = Random.Range(0, templates.bottomRooms.Length);
-                GameObject newObject = Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                GameObject newObject = Instantiate(templates.bottomRooms[rand], transform.position + fixup, templates.bottomRooms[rand].transform.rotation);
                 newObject.transform.SetParent(parentObject);
             }
             else if (openingDirection == 3)
             {
                 // spawn room with right door
                 rand = Random.Range(0, templates.rightRooms.Length);
-                GameObject newObject = Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                GameObject newObject = Instantiate(templates.rightRooms[rand], transform.position + fixleft, templates.rightRooms[rand].transform.rotation);
                 newObject.transform.SetParent(parentObject);
             }
             else if (openingDirection == 4)
             {
                 //spawn room with left door
                 rand = Random.Range(0, templates.leftRooms.Length);
-                GameObject newObject = Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                GameObject newObject = Instantiate(templates.leftRooms[rand], transform.position + fixright, templates.leftRooms[rand].transform.rotation);
                 newObject.transform.SetParent(parentObject);
             }
             spawned = true;
