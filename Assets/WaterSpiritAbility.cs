eb52c29250faef8093cd8a7cdd277bed2af85be7
k2211326 <k2211326@kingston.ac.uk>
2025-02-24 11:58:12 +0000

    Added in Spritits and some assets
    FireSpirit
    WaterSpirit
    Boulder ability
    Water wall ability

---
+++ Assets/WaterSpiritAbility.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework.Internal.Commands;
+using UnityEngine;
+
+public class WaterSpiritAbility : MonoBehaviour
+{
+    // heals the player over time when called
+    // dies after a while
+    void Start()
+    {
+
+    }
+
+}
