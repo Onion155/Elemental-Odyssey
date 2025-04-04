d3b7723c5805a52eb0e959f029529d77d9d944b0
k2211326 <k2211326@kingston.ac.uk>
2025-04-04 16:59:52 +0100

    The Doors now lead to the next level

--- Assets/Scripts/PlayerStuff/GateControler.cs
+++ Assets/Scripts/PlayerStuff/GateControler.cs
@@ -1,14 +1,21 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GateControler : MonoBehaviour
 {
+   public string scene;
     // this should teleport the player to the next room or to the boss roo
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // teleports player 
+            SceneManager.LoadScene(scene);
+            Debug.Log("Hello from trigger");
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hello from collision");
     }
 }
